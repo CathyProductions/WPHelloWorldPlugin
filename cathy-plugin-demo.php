f14eed995903effed3d94ae667721f7e5bae5938
CathyProductions <35899622+CathyProductions@users.noreply.github.com>
2018-01-28 19:42:48 +0100

    added demo action and filter

--- cathy-plugin-demo.php
+++ cathy-plugin-demo.php
@@ -8,3 +8,16 @@ Author URI: https://www.cathyprod.de
 License: GPL
 */
 ?>
+
+add_action('wp_footer', 'cathy_plugin_demo_action_footer');
+function cathy_plugin_demo_action_footer() {
+  echo '<div>Plugin Demo Add Footer "Hello World"</div>';
+}
+
+add_filter('the_content', 'cathy_plugin_demo_filter_content');
+function cathy_plugin_demo_filter_content($content) {
+  $content .= '<p>Plugin Demo Add One Line - "Hello World"</p>';
+  return $content;
+}
+
+?>
