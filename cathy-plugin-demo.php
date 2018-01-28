2cb1e2d2dbed31b9ea5fa8887bde80606abad64a
CathyProductions <35899622+CathyProductions@users.noreply.github.com>
2018-01-28 19:41:14 +0100

    plugin-demo.php comment section

---
+++ cathy-plugin-demo.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Cathy Plugin Demo
+Description: WordPress "Hello World"-Plugin-Demo
+Version: 0.1
+Author: CathyProductions
+Author URI: https://www.cathyprod.de
+License: GPL
+*/
+?>
